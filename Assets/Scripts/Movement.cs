6773d56046ffa2d3afd80b3fcfe6116b4731a086
Daiyamon <ggtvgamer46@gmail.com>
2024-10-19 15:07:02 +0700

    Add Project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer spriteRenderer;
+
+    public float x;
+    public int speed = 2;
+    //public int jumpingPower = 2;
+    public bool walk = false;
+    public bool run = false;
+    void Start()
+    {
+        Transform transform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            run = true;
+        }
+        else
+        {
+            run= false;
+        }
+
+        if (x != 0 && run)
+        {
+            anim.SetBool("Run", true);
+        }
+        else if(!run)
+        {
+            anim.SetBool("Run", false);
+        }
+        else if (x!=0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
